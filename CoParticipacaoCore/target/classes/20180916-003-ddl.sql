15efaad6b275a0cf4a8c4fbacfd0689aad56e27a
edson.apereira <edson.apereira@INV002917-.spread.tecnologia>
2018-10-25 12:18:52 -0300

    1) atualizando;

--- CoParticipacaoCore/target/classes/20180916-003-ddl.sql
+++
@@ -1,7 +0,0 @@
-/**
- * Edson - 16/09/2018
- */
-
-alter table TB_REGRA
-	modify NM_REGRA varchar( 400 ) not null;
-	
--- CoParticipacaoReport/src/main/java/br/com/spread/qualicorp/webservice/coparticipacaoreport/ObjectFactory.java
+++ CoParticipacaoReport/src/main/java/br/com/spread/qualicorp/webservice/coparticipacaoreport/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2018.10.24 às 11:47:46 AM BRT 
+// Gerado em: 2018.10.25 às 12:03:15 PM BRT 
 //
 
 
